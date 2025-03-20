3b499874f235d82a0b217975f98b3f34bc7e0567
Vaibhav <vaibhav.sharma3100@gmail.com>
2025-03-20 10:42:30 +0530

    updateInfo endpoint

--- Backend/middleware.js
+++ Backend/middleware.js
@@ -1,8 +1,31 @@
-const z = require('zod');
+  
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET} = require('../config.js');
 
-export const UserSchema = z.object({
-    username: z.string().email(),
-    password: z.string().min(8),
-    Firstname: z.string(),
-    Lastname: z.string()
-})
+
+
+
+const authMidddleware = (req,res,next) =>{
+    const authHeader = req.header.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer')){
+        return res.status(403).json({});
+    }
+
+    const token = authHeader.split(' ')[1];
+    try{
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        if(decoded.userId){
+            req.user = decoded.userId;
+            next();
+        }else{
+            return res.status(403).json({});
+        }
+        
+    }catch(err){
+        return res.status(403).json({});
+    }
+};
+
+module.exports = authMidddleware;
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -3,11 +3,19 @@ const router = express.router();
 const User = require("../models/user.js");
 const jwt = require("jsonwebtoken");
 const {JWT_SECRET} = require("../config.js");
-const UserSchema = require("../schema/user.js");
+const z = require("zod");
+const {middleware} = require("../middleware.js");
+const authMidddleware = require("../middleware.js");
 
 
+const UserSchema = z.object({
+  username: z.string().email(),
+  password: z.string().min(8),
+  Firstname: z.string(),
+  Lastname: z.string()
+})
 
-app.post("/signup", async(req,res)=>{
+router.post("/signup", async(req,res)=>{
 
     const body = req.body;
 
@@ -34,18 +42,66 @@ app.post("/signup", async(req,res)=>{
         message: "User created successfully",
         token: token
     })
+}) 
+
+const UserSchemaSignin = z.object({
+  username: z.string().email(),
+  password: z.string().min(8)
+}) 
+
+router.post("/signin", async(req,res)=>{
+
+    const {parsedBody} = UserSchemaSignin.safeParse(req.body);
+
+    if(!parsedBody){
+      return res.status(400).send("Incorrect inputs");
+   }
+
+   const user = await User.findOne({username: req.body.username, password: req.body.password});
+
+   if(user){
+   const token = jwt.sign({
+    userId : dbUser._id,
+
+  }, JWT_SECRET);
+  res.json({
+      token: token
+  })
+  return;
+   }
+   
 })
 
-app.post("signin", (req,res)=>{
-    const body = req.body;
+const updateBody = z.object({
+  password: Zod.string().optional(),
+  FirstName: Zod.string().optional(),
+  Lastname: Zod.string().optional(),
 
-    
+})
 
+router.put("/", authMidddleware, async(req,res)=>{
+  const {parsedBody} = UserSchema.safeParse(req.body);
+
+  if(!parsedBody){
+     return res.status(400).send("Email already taken / Incorrect inputs");
+  }
+
+  await User.updateOne(req.body, {
+    id: req.userId
+  })
+
+  res.json({
+    message: "Updated Successfully"
+  })
 })
 
 
-app.post("signup", (req,res)=>{
 
-})
+
+
+
+
+
+
 
 module.exports = router;
